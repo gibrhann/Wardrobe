39686fbb75116c83a3f15be24ef37b4218d37ca9
gibrhann <groman88@gmail.com>
2020-05-02 17:01:51 -0500

    Initial Commit

---
+++ Wardrobe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wardrobe
+//
+//  Created by Coates-C02X76QXJHC9 on 5/2/20.
+//  Copyright © 2020 Gibrhann Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
